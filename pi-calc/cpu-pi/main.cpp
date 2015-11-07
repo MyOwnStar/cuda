d9c2ab0e909204c88c31dab3acb4971cd6dbdbed
MyOwnStar <mashenkoalex@gmail.com>
2015-11-07 16:36:00 +0900

    add Pi calc on CPU

---
+++ pi-calc/cpu-pi/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <cmath>
+
+
+double f(double x)
+{
+   return 4 / (1.0 + x * x);
+}
+
+double calcIntegral(unsigned long n)
+{
+   double h = 1.0 / static_cast<double>(n);
+
+   double sum = 0.0;
+   for (size_t i = 0; i < n; i++)
+   {
+      double x = (i + 0.5) * h;
+      sum += f(x);
+   }
+
+   return sum / n;
+}
+
+
+int main()
+{
+   const double PI = 3.141592653589793;
+
+   unsigned long stepCount = 500000000;
+
+   typedef std::chrono::steady_clock::time_point Time;
+
+   std::cout << "Computing... ";
+   Time start = std::chrono::steady_clock::now();
+   double result = calcIntegral(stepCount);
+   Time stop = std::chrono::steady_clock::now();
+   std::cout << "OK" << std::endl;
+
+   std::cout << "Result: " << std::setprecision(15) << std::fixed << result << std::endl;
+   std::cout << "Error: " << fabs(result - PI) << std::endl;
+   std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
+             << " ms" << std::endl;
+
+   return 0;
+}
